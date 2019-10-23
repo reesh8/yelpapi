7bbfc172ae232a3b1cc25addaf2fd18f4e8d5a07
reesh8 <reesh8@gmail.com>
2019-10-23 15:22:47 -0400

    Update yelp.py

--- yelp.py
+++ yelp.py
@@ -4,7 +4,7 @@ URL="https://api.yelp.com/v3/businesses/search"
 #categories="chinese"
 params={"term": "restaurants", "location": "Manhattan, New York City"}
 
-headers={"Authorization": "Bearer o7KiYRSz2gnLm142gBwbN5Qt-_yyUbw7CqCRC4jdUNT97K4574b2HuOZgtkiWNCbW4s8LqUcqLXX27s_fU1_QHGGDUOz4TspT-o0ylvla2N19XnlduFvrkvV-jGSXXYx"}
+headers={"Authorization": "Bearer "}
 r = requests.get(url = URL, params = params, headers=headers) 
 data=r.json()
 #print data
